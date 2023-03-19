0d2ab0ed9b14a22cb5183a3fefa8fede1d8bac80
Solanki Gaurav <solankigaurav2107@gmail.com>
2023-03-19 15:03:01 +0530

    graph functionality done

--- main.js
+++ main.js
@@ -1,30 +1,50 @@
 const { app, BrowserWindow } = require("electron");
+const mysql = require("mysql2/promise");
+async function createWindow() {
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "stock_portfolio",
+  });
 
-const createWindow = () => {
+  const [rows, fields] = await connection.query(
+    "SELECT date, daily_pl FROM daily_pl"
+  );
+
+  const chartData = {
+    labels: rows.map((row) => row.date),
+    datasets: [
+      {
+        label: "Daily PL",
+        data: rows.map((row) => row.daily_pl),
+        borderColor: "rgba(255, 99, 132, 1)",
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
   const win = new BrowserWindow({
+    width: 800,
+    height: 600,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     },
-    width: 800,
-    height: 600,
   });
 
   win.loadFile("index.html");
-};
 
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
+  win.webContents.on("did-finish-load", () => {
+    win.webContents.executeJavaScript(`
+      const ctx = document.getElementById('chart').getContext('2d');
+      new Chart(ctx, {
+        type: 'line',
+        data: ${JSON.stringify(chartData)}
+      });
+    `);
   });
-});
+}
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
+app.whenReady().then(createWindow);
