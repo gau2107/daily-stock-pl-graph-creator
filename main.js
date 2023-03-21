8009f826fa1ffcbd1b2b14afb5068c33d71224fd
Solanki Gaurav <solankigaurav2107@gmail.com>
2023-03-21 18:38:17 +0530

    add cur_value and total_pl chart

--- main.js
+++ main.js
@@ -9,15 +9,41 @@ async function createWindow() {
   });
 
   const [rows, fields] = await connection.query(
-    "SELECT date, daily_pl FROM daily_pl"
+    "SELECT * FROM daily_pl"
   );
 
-  const chartData = {
+  const chartDataDailyPl = {
     labels: rows.map((row) => new Date(row.date).toDateString()),
     datasets: [
       {
         label: "Daily PL",
         data: rows.map((row) => row.daily_pl),
+        borderColor: "rgba(255, 99, 132, 1)",
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const chartDataTotalPl = {
+    labels: rows.map((row) => new Date(row.date).toDateString()),
+    datasets: [
+      {
+        label: "Total PL",
+        data: rows.map((row) => row.total_pl),
+        borderColor: "rgba(255, 99, 132, 1)",
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const chartDataCurrentValue = {
+    labels: rows.map((row) => new Date(row.date).toDateString()),
+    datasets: [
+      {
+        label: "Current Value",
+        data: rows.map((row) => row.current_value),
         borderColor: "rgba(255, 99, 132, 1)",
         backgroundColor: "rgba(255, 99, 132, 0.2)",
         borderWidth: 1,
@@ -38,10 +64,26 @@ async function createWindow() {
 
   win.webContents.on("did-finish-load", () => {
     win.webContents.executeJavaScript(`
-      const ctx = document.getElementById('chart').getContext('2d');
+      const ctx = document.getElementById('daily-chart').getContext('2d');
       new Chart(ctx, {
         type: 'line',
-        data: ${JSON.stringify(chartData)}
+        data: ${JSON.stringify(chartDataDailyPl)}
+      });
+    `);
+
+    win.webContents.executeJavaScript(`
+      const ctx1 = document.getElementById('total-chart').getContext('2d');
+      new Chart(ctx1, {
+        type: 'line',
+        data: ${JSON.stringify(chartDataTotalPl)}
+      });
+    `);
+
+    win.webContents.executeJavaScript(`
+      const ctx2 = document.getElementById('current-chart').getContext('2d');
+      new Chart(ctx2, {
+        type: 'line',
+        data: ${JSON.stringify(chartDataCurrentValue)}
       });
     `);
   });
