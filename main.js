cd4d6dfd0590e5d48dcb5173502880e85a5a33b2
Solanki Gaurav <solankigaurav2107@gmail.com>
2023-03-29 18:56:18 +0530

    remove unnecessary code

--- main.js
+++ main.js
@@ -23,20 +23,7 @@ async function createWindow() {
         borderWidth: 1,
       },
     ],
-  };
-
-  const chartDataTotalPl = {
-    labels: rows.map((row) => new Date(row.date).toDateString()),
-    datasets: [
-      {
-        label: "Total PL",
-        data: rows.map((row) => row.total_pl),
-        borderColor: "rgba(255, 99, 132, 1)",
-        backgroundColor: "rgba(255, 99, 132, 0.2)",
-        borderWidth: 1,
-      },
-    ],
-  };
+  };;
 
   const chartDataCurrentValue = {
     labels: rows.map((row) => new Date(row.date).toDateString()),
@@ -71,13 +58,7 @@ async function createWindow() {
       });
     `);
 
-    win.webContents.executeJavaScript(`
-      const ctx1 = document.getElementById('total-chart').getContext('2d');
-      new Chart(ctx1, {
-        type: 'line',
-        data: ${JSON.stringify(chartDataTotalPl)}
-      });
-    `);
+    
 
     win.webContents.executeJavaScript(`
       const ctx2 = document.getElementById('current-chart').getContext('2d');
